07027c70a500b2100ea8ea894219f11af535222f
Liao wenchiy <jjliao8889@gmail.com>
2021-01-31 23:06:54 +0800

    added pause feature

--- js/main.js
+++ js/main.js
@@ -1,113 +1,140 @@
-//manually input
+let time = 25,
+	status = "stop";
 
-//get riped tomatos
-
-//change color
-
-//preset
-let time = 25;
-let display = document.getElementById("display"),
+const display = document.getElementById("display"),
 	stopBtn = document.getElementById("stop"),
 	addBtn = document.getElementById("add"),
 	decreaseBtn = document.getElementById("decrease"),
-	startBtn = document.getElementById("start");
-tomato = document.querySelector("#greenTomato  .cls-1");
+	startBtn = document.getElementById("start"),
+	pauseBtn = document.getElementById("pause"),
+	tomato = document.querySelector("#greenTomato  .cls-1"),
+	minInput = document.getElementById("minInput");
 
-//install eventhandlers
-addBtn.onclick = function () {
-	changeTime(1);
-};
-decreaseBtn.onclick = function () {
-	changeTime(-1);
-};
-startBtn.onclick = function () {
-	timer(time);
-};
+addBtn.onclick = () => changeTime(1);
+decreaseBtn.onclick = () => changeTime(-1);
+startBtn.onclick = () => startCountdown(time);
+display.onclick = openInput;
+minInput.onblur = closeInput;
 
-// display.onclick = manuallyInput;
-
-function manuallyInput(e) {
-	console.dir(e.currentTarget);
-	e.currentTarget.outerHTML = `<input class="manuallyInput" type="text" value="${e.currentTarget.innerHTML}"/ >`;
+function openInput(e) {
+	if (status === "stop") {
+		minInput.value = time;
+		minInput.classList.toggle("hide");
+		minInput.focus();
+		display.textContent = ": 00";
+	}
 }
 
-//input time
+function closeInput() {
+	time = Number(minInput.value);
+	display.textContent = time + " : 00";
+	minInput.classList.toggle("hide");
+}
+
 function changeTime(par) {
-	if (par === 1) {
-		time += 1;
-	} else {
-		time -= 1;
-	}
+	time += par;
+
 	if (time > 0) {
-		display.innerHTML = time + " : 00";
+		display.textContent = time + " : 00";
 	} else {
 		alert("請至少專注一分鐘！");
 		time += 1;
 	}
 }
 
-//start-stop switch
-function toggleBtns() {
-	stopBtn.classList.toggle("hide");
-	startBtn.classList.toggle("hide");
-	addBtn.classList.toggle("hide");
-	decreaseBtn.classList.toggle("hide");
-}
-
-//timer
-function timer(min) {
-	toggleBtns();
+function startCountdown(min) {
+	status = "counting";
 	let duration = min * 60,
 		sec = 0,
 		count = 0,
 		col = 0,
 		timeoutId = setInterval(countdown, 1000);
 
-	//countdown
+	toggleBtns();
+	pauseBtn.onclick = pauseCountdown;
+	stopBtn.onclick = reset;
+
 	function countdown() {
-		//logic
-		if (sec == 0) {
-			min -= 1;
-			sec = 60;
+		countTime();
+		updateView();
+		handleTimesUp();
+
+		function countTime() {
+			if (sec === 0) {
+				min -= 1;
+				sec = 60;
+			}
+			sec -= 1;
+			count++;
 		}
-		sec -= 1;
-		count++;
 
-		//display remaining time
-		if (sec < 10) {
-			display.innerHTML = min + " : 0" + sec;
-		} else {
-			display.innerHTML = min + " : " + sec;
+		function updateView() {
+			//display remaining time
+			if (sec < 10) {
+				display.textContent = min + " : 0" + sec;
+			} else {
+				display.textContent = min + " : " + sec;
+			}
+			document.title = String(display.textContent);
+
+			//change color
+			col = 120 * (1 - count / duration);
+			tomato.style.fill = `hsl(${col},80%,50%)`;
 		}
-		document.title = String(display.innerHTML);
 
-		//change color
-		col = 120 * (1 - count / duration);
-		tomato.style.fill = `hsl(${col},100%,50%)`;
-
-		//stop countdown
-		if (min + sec === 0) {
-			clearInterval(timeoutId);
-			display.innerHTML = "Here's your tomato!";
-			document.title = "Time's Up!";
-			stopBtn.innerHTML = "Try again!";
+		function handleTimesUp() {
+			if (min + sec === 0) {
+				status = "stop";
+				clearInterval(timeoutId);
+				display.textContent = "Here's your tomato!";
+				document.title = "Time's Up!";
+				stopBtn.textContent = "Try again!";
+				pauseBtn.classList.toggle("hide");
+			}
 		}
 	}
 
-	//reset timer, display, color
-	stopBtn.onclick = reset;
-	function reset() {
-		if (
-			confirm("你確定要放棄嗎?") ||
-			min + sec == 0 //when try again
-		) {
+	function toggleBtns() {
+		stopBtn.classList.toggle("hide");
+		startBtn.classList.toggle("hide");
+		addBtn.classList.toggle("hide");
+		decreaseBtn.classList.toggle("hide");
+		pauseBtn.classList.toggle("hide");
+	}
+
+	function pauseCountdown() {
+		if (status === "counting") {
+			pauseBtn.textContent = "Resume";
+			status = "pausing";
+			pauseBtn.onclick = resumeCountdown;
 			clearInterval(timeoutId);
+			pauseBtn.classList.toggle("active");
+		}
+	}
+
+	function resumeCountdown() {
+		if (status === "pausing") {
+			status = "counting";
+			pauseBtn.textContent = "Pause";
+			pauseBtn.onclick = pauseCountdown;
+			timeoutId = setInterval(countdown, 1000);
+			pauseBtn.classList.toggle("active");
+		}
+	}
+
+	function reset() {
+		if (min + sec === 0 || confirm("你確定要放棄嗎?")) {
+			clearInterval(timeoutId);
+			status = "stop";
 			time = 25;
-			display.innerHTML = time + " : 00";
-			document.documentElement.style.setProperty("--tomatoColor", "hsl(120,100%,50%)");
+
+			display.textContent = time + " : 00";
+			stopBtn.textContent = "Stop";
+			tomato.style.fill = `hsl(120,80%,50%)`;
+
 			toggleBtns();
+			pauseBtn.classList.add("hide");
 			document.title = "TOMATO TIMER";
-			stopBtn.innerHTML = "Stop";
 		}
 	}
 }
